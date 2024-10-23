c52f339c9365264f0e832070bcb78dec09bda290
Stef Van Nieuwenhove <stef.vannieuwenhove@student.hogent.be>
2024-10-23 15:04:20 +0200

    create a new planner as delete a planner & loading and error page for planner and home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Navbar } from '@/components';
 import { ClerkProvider } from '@clerk/nextjs';
+import { Toaster } from '@/components/ui/sonner';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
           <main className='container mx-auto min-h-screen h-full my-3'>
             {children}
           </main>
+          <Toaster richColors duration={5000} theme='light' />
           <footer></footer>
         </body>
       </html>
